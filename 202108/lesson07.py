4c8a09450375e71a0e55f4e03be7b946fd1eb73d
mafei <zhdn_791005@163.com>
2021-08-09 09:03:55 +0800

    add lesson7

---
+++ 202108/lesson07.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2021/8/9 8:46
+# @Author : ma.fei
+# @File : lesson07.py.py
+# @Software: PyCharm
+
+# 导入数学库
+import math
+
+# 打印圆周率
+print(math.pi)
+
+# 输出字符串
+print('hello world!')
+
+# 输出字符串，带换行符'\n'
+print('apple\nbanana\pear\n')
+
+# 数学运算
+
+# 加法
+print(2+2)
+
+# 减法
+print(2-2)
+
+# 乖法
+print(2*2)
+
+# 除法
+print(2/2)
+
+# 求余数
+print(2%2)
+
+# 幂运算
+print(2**2)
+
